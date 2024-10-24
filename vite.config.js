5ed2845d02a9d8ea2b88f8312ed903041350c3e6
sumaiya <you@example.com>
2024-10-24 07:40:47 +0600

    test

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import fs from 'fs';
+
+// Function to get all HTML files dynamically
+function getHtmlFiles(dir) {
+    // base: '/khl/'; //এটা github এ আপনার dist folder আলাদা branch এ subtree করে deploy করার জন্য মাকঝকানে নামটা repo name
+    let files = fs.readdirSync(dir);
+    let htmlFiles = {};
+
+    files.forEach(file => {
+        if (file.endsWith('.html')) {
+            htmlFiles[file.replace('.html', '')] = resolve(dir, file);
+        }
+    });
+    return htmlFiles;
+}
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: getHtmlFiles('./') // Current directory (./) থেকে সব html ফাইল ইনপুট হিসেবে নেয়া হচ্ছে
+        }
+    }
+});
